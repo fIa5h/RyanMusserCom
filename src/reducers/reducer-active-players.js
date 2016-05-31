c308921c7f59f5807fa1dd78eb4cc72dbe071365
RyanThomasMusser <ryanthomasmusser@gmail.com>
2016-05-30 23:23:59 -0700

    add blog links

--- src/reducers/reducer-active-players.js
+++ src/reducers/reducer-active-players.js
@@ -5,24 +5,14 @@ export default function( state = [] , action ){
   switch (action.type) {
     case 'PLAYER_SELECTED':
 
-      console.log('....');
-      console.log('....');
-      console.log('....');
-      console.log('....');
-
-      console.log('starting state');
-      console.log(state);
-      console.log('....');
-
       if(state.length === 0){
 
-        //return the new state
-        return state.concat(action.payload);
+        //return the updated state
+        return [ action.payload, ...state ];
 
       }else{
 
         //let's see if the player exists in the array
-        var newState = [];
         var playerExistsInArray = 0;
         var indexToRemove = 0;
         state.map(function (player, index) {
@@ -34,16 +24,8 @@ export default function( state = [] , action ){
         });
 
         if(playerExistsInArray === 0){
-          return state.concat(action.payload);
+          return [ action.payload, ...state ];
         }else{
-          //console.log('removed player!');
-          // state.splice(indexToRemove,1);
-          // console.log('spliced product ^');
-          // console.log(state.slice(indexToRemove,indexToRemove+1));
-          // console.log('sliced product ^');
-          // console.log(state);
-          // console.log('updated state');
-          // return state.slice(indexToRemove,indexToRemove+1);
           return [...state.slice(0, indexToRemove), ...state.slice(indexToRemove + 1)];
         }
 
